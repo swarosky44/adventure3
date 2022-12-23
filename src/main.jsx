730572988e6dfa5ff1fa804baa421274a80ce571
swarosky44 <swarosky44@gmail.com>
2022-12-23 15:02:12 +0800

    feat:调整

--- src/main.jsx
+++ src/main.jsx
@@ -10,8 +10,8 @@ import '@rainbow-me/rainbowkit/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <WagmiConfig client={client}>
-      <RainbowKitProvider chains={chains}>
-        <RouterProvider router={routes} />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <RainbowKitProvider chains={chains}>
+      <RouterProvider router={routes} />
+    </RainbowKitProvider>
+  </WagmiConfig>
 )
