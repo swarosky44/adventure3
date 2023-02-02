19dfa17c647e0ec5098ca7be012d7e4766e23fe3
swarosky44 <swarosky44@gmail.com>
2023-02-02 19:35:22 +0800

    feat: 调整

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,8 +1,9 @@
 import { useCallback } from 'react'
-// import { useSearchParams } from 'react-router-dom'
+import { useSearchParams, useNavigate } from 'react-router-dom'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import Particles from 'react-particles'
 import { loadFull } from 'tsparticles'
+import { PlusOutlined } from '@ant-design/icons'
 import particlesOpt from './particles.json'
 import BasicPannel from './components/BasicPannel'
 import DescPannel from './components/DescPannel'
@@ -10,8 +11,10 @@ import RewardPannel from './components/RewardPannel'
 import styles from './index.module.less'
 
 const Detail = () => {
-  // const [params] = useSearchParams()
-  // const id = params.get('id')
+  const navigate = useNavigate()
+  const [params] = useSearchParams()
+  const id = params.get('id')
+  console.info(id)
 
   const particlesInit = useCallback(async (engine) => {
     await loadFull(engine)
@@ -27,6 +30,10 @@ const Detail = () => {
         <img className={styles.logo} src="https://db35z3hw6fbxp.cloudfront.net/detail-logo.png" />
         <div className={styles.right}>
           <ConnectButton />
+          <div className={styles.create} onClick={() => navigate('/backend')}>
+            <PlusOutlined style={{ marginRight: '8px' }} />
+            New Adventure
+          </div>
         </div>
       </header>
       <main className={styles.module}>
